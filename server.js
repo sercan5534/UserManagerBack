88b892d18679dc84ece955afe22de6887daf4953
Sercan <sozen@netas.com.tr>
2016-09-11 17:18:43 +0300

    fixed post err

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
+    //res.header("Access-Control-Allow-Methods","GET, POST, PUT, DELETE");
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods","GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 //LOGGING
