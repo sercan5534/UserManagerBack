70ce31f3356fd2a848878d1c24624546bb6a20e6
Sercan <sozen@netas.com.tr>
2016-09-11 17:48:36 +0300

    use module for cors

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 //APP
 var express = require('express');
-var cors = require('cors')
 var app = express();
 var bodyParser  = require('body-parser');
 var morgan      = require('morgan');
@@ -12,13 +11,21 @@ require('./app.exceptions');
 
 app.set('port', (process.env.PORT || 5000));
 
-app.options('/', function (req, res) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader('Access-Control-Allow-Methods', '*');
-    res.setHeader("Access-Control-Allow-Headers", "*");
-    res.end();
-});
+//CROSS
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+};
+app.use(allowCrossDomain);
 
 //BODY PARSING
 app.use(bodyParser.urlencoded({ extended: false }));
